278414f14760c0887342978643ca44f84014b389
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2025-08-20 10:34:55 +0330

    c

--- apps/persianpart-api/src/libs/db.ts
+++ apps/persianpart-api/src/libs/db.ts
@@ -24,7 +24,7 @@ export async function initializeDB() {
 export async function addRootUser() {
   const addRootUserLogger = dbLogger.scope('addRootUser');
 
-  if (((await UserModel.count?.()) ?? 0) >= 1) {
+  if (((await UserModel.countDocuments()) ?? 1) >= 0) {
     const user = await UserModel.findOne({});
 
     addRootUserLogger.success('User:', user);
@@ -34,28 +34,36 @@ export async function addRootUser() {
 
   addRootUserLogger.info(`addRootUser`);
 
-  const user = await UserModel.create({
-    firstName: 'محمدمهدی',
-    lastName: 'زمانیان',
-    password: 'mmz1384',
-    permission: 'root',
-    phoneNumber: '09155595488',
-  });
-
-  addRootUserLogger.success('User Created:', user);
+  try {
+    const user = await UserModel.create({
+      firstName: 'محمدمهدی',
+      lastName: 'زمانیان',
+      password: 'mmz1384',
+      permission: 'root',
+      phoneNumber: '09155595488',
+    });
+  
+    addRootUserLogger.success('User Created:', user);
+  } catch (error) {
+    addRootUserLogger.error('addRootUser Error:', error);
+  }
 }
 export async function addSetting() {
-  if (((await SettingModel.count?.()) ?? 1) > 0) return;
+  if (((await SettingModel.countDocuments()) ?? 1) >= 0) return;
 
   const addSettingLogger = dbLogger.scope('addSetting');
 
   addSettingLogger.info(`addSetting`);
 
-  const setting = await SettingModel.create({
-    status: 'up',
-  });
-
-  addSettingLogger.success('Setting Created:', setting);
+  try {
+    const setting = await SettingModel.create({
+      status: 'up',
+    });
+  
+    addSettingLogger.success('Setting Created:', setting);
+  } catch (error) {
+    addSettingLogger.error('addSetting Error:', error);
+  }
 }
 
 initializeDB()
