299355514162cc8d03aa25ed9eed8bc8703cf580
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2026-05-26 19:51:09 +0330

    c

--- apps/persianpart-pwa/src/client.ts
+++ apps/persianpart-pwa/src/client.ts
@@ -10,7 +10,7 @@ export const api = createTRPCProxyClient<AppRouter>({
   links: [
     errorHandlingLink,
     httpLink({
-      url: '//api-persianpart.g360.ir',
+      url: 'https://api.persianpartshop.ir',
       fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => {
         request('loading', 'start');
 
